a88213fe5e2acc7b9e81b9c82f0c8255c7292d47
laigasus <okjaeook98@gmail.com>
2024-11-12 13:56:59 +0900

    chore: SecurityConfig 설정 변경
    `+` 하위 도메인에서 인증을 수행하도록 bypass

--- src/main/java/org/sesac/market/config/SecurityConfig.java
+++ src/main/java/org/sesac/market/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
@@ -16,6 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
 
+@EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
@@ -28,6 +31,11 @@ public class SecurityConfig {
 
     @Value("${okta.oauth2.client-id}")
     private String clientId;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/account/**", "/product/**", "order/**");
+    }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
