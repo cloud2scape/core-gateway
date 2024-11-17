85f7dd98c7f70df32e1b67ad628b42e1ad19e97a
laigasus <okjaeook98@gmail.com>
2024-11-17 19:31:58 +0900

    feat: gateway controller 엔드포인트 추가/변경
    `+` home
    `#` verbose services

--- src/main/java/org/sesac/market/controller/GatewayController.java
+++ src/main/java/org/sesac/market/controller/GatewayController.java
@@ -2,11 +2,17 @@ package org.sesac.market.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,9 +20,30 @@ public class GatewayController {
 
     private final DiscoveryClient discoveryClient;
 
+    @GetMapping("/")
+    public ResponseEntity<?> home() throws IOException {
+
+        Map<String, Object> info = Map.ofEntries(
+                Map.entry("timestamp", LocalDateTime.now()),
+                Map.entry("description", "SeSAC Market API Gateway 🌼"),
+                Map.entry("ip", InetAddress.getLocalHost().getHostAddress())
+        );
+
+        return ResponseEntity.ok(info);
+    }
+
     @GetMapping("/services")
-    public List<String> services() {
-        return discoveryClient.getServices();
+    public ResponseEntity<?> services() {
+        Map<String, List<String>> services = new LinkedHashMap<>();
+
+        discoveryClient.getServices().forEach(service -> {
+            List<String> instances = discoveryClient.getInstances(service).stream()
+                    .map(instance -> instance.getUri().toString())
+                    .toList();
+            services.put(service, instances);
+        });
+
+        return ResponseEntity.ok(services);
     }
 
     @GetMapping("/login")
